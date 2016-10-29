68ae93289438816dde69ff22653f9ec10ff4e0ae
datatribe <datatribe@gmail.com>
2016-10-29 17:18:57 -0400

    Basic test and object files setup and ready to start coding

--- src/Test/TestCases.java
+++ src/Test/TestCases.java
@@ -1,10 +1,55 @@
 package Test;
 
-import org.junit.Test;
+import com.datatribe.kata.RomanNumeralConverter;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
 
 /**
- * Created by domin on 10/28/2016.
+ * Created by datatribe on 10/28/2016.
  */
 public class TestCases {
+    // public test methods
 
+
+    // using Junit 4.12
+
+    /* These methods are here if needed - grabbed from sample
+    // run once before any test cases
+    @BeforeClass
+    public static void runOnceBeforeClass(){
+        // if we use any connection pools, etc. that are needed for all tests
+        System.out.println("Setting up test run...\r\n");
+    }
+
+    @AfterClass
+    public static void runOnceAfterClass(){
+        // tear down class members
+        System.out.println("Tearing down after tests have ran.\r\n");
+    }
+
+    // Any operations needed before each test method, eg reinitializing class members
+    @Before
+    public void runBeforeTestMethod(){
+        //
+        System.out.println("Pre Test Case Operations...\r\n");
+    }
+
+    // Any cleanup after each test
+    @After
+    public void runAfterEachTest(){
+        //
+        System.out.println("Post Test Case Operations\r\n");
+    }
+    // private test globals
+ */
+
+    // test methods
+    @Test
+    public void testObjectCanConstruct(){
+        System.out.println("Test construct RomanNumeralConverter");
+        RomanNumeralConverter romanNumeralConverter = new RomanNumeralConverter();
+        assertEquals(true, romanNumeralConverter!=null);
+    }
 }
---
+++ src/com/datatribe/kata/RomanNumeralConverter.java
@@ -0,0 +1,19 @@
+package com.datatribe.kata;
+
+/**
+ * Created by datatribe on 10/29/2016.
+ */
+public class RomanNumeralConverter {
+    // public declarations
+
+    // private declarations
+
+    // constructor
+    public RomanNumeralConverter(){
+       // object setup as needed
+    }
+
+    // public methods
+
+    // private methods
+}
